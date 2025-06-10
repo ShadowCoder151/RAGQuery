ab23c10a08b852a1b375d933acd77ba375ebcfe2
ShadowCoder151 <shadowcoder023@gmail.com>
2025-06-10 21:16:51 +0530

    feat: update embedding.py with langchain implementation

--- src/embedding.py
+++ src/embedding.py
@@ -1,26 +1,25 @@
 # Loading the PDF, chunking and embedding
 
-from sentence_transformers import SentenceTransformer
-from pypdf import PdfReader
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
 
-def PDFLoadText(pdf_path: str) -> str:
-    reader = PdfReader(pdf_path)
-    return " ".join(page.extract_text() for page in reader.pages)
+def PDFLoadText(pdf_path: str):
+    reader = PyPDFLoader(pdf_path)
+    return reader.load()
 
-def ChunkText(text: str, size: int=500, overlap: int=100):
-    words = text.split()
-    chunks = []
+def ChunkText(docs, size: int=500, overlap: int=100):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=size, chunk_overlap=overlap)
+    return splitter.split_documents(docs)
 
-    for i in range(0, len(words), size-overlap):
-        chunks.append(" ".join(words[i:i + size]))
-    
-    return chunks
+def embed_chunks(chunks: list[str], model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    vec_store = FAISS.from_documents(chunks, embedding=embeddings)
+    return vec_store
 
-def embed_chunks(chunks: list[str], model_name="all-MiniLM-L6-v2"):
-    model = SentenceTransformer(model_name)
-    return model.encode(chunks, show_progress_bar=True)
 
 
 # Testing using a sample PDF file
 pdf_path = "data\\study_guide.pdf"
-print(PDFLoadText(pdf_path=pdf_path))
+
